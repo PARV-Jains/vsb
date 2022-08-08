f6fa8e4031592f6d1139c5d11a62585bb6951cdb
PARV-Jains <2006parvjain@gmail.com>
2022-08-08 10:31:26 +0530

    env

--- utils/client.js
+++ utils/client.js
@@ -1,8 +1,8 @@
 import sanityClient from '@sanity/client';
 import config from './config';
 const client = sanityClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  projectId: config.projectId,
+  dataset: config.dataset,
   useCdn: true,
    apiVersion: '2022-05-30',
   tokenWithWriteAccess: process.env.SANITY_AUTH_TOKEN
