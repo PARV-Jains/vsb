650a8644813c1fa6f49ac936794c782984eeaec8
PARV-Jains <2006parvjain@gmail.com>
2022-08-21 12:56:32 +0530

    pwa again

--- next.config.js
+++ next.config.js
@@ -1,16 +1,15 @@
+const withPWA = require("next-pwa");
 // /** @type {import('next').NextConfig} */
-const withPWA = require("next-pwa"); 
-module.exports = withPWA({
+module.exports =  withPWA({
   reactStrictMode: true,
-  images:{
-    domains: ['cdn.sanity.io']
+  images: {
+    domains: ['cdn.sanity.io'],
   },
   pwa: {
     dest: "public",
     register: true,
     skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
   },
 });
-
-
-
+// module.exports = nextConfig;
