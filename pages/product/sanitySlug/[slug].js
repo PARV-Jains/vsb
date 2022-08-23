747aef60739141209b27524b069e1458069fc5f4
PARV-Jains <2006parvjain@gmail.com>
2022-08-23 19:10:53 +0530

    ....

--- pages/product/sanitySlug/[slug].js
+++ pages/product/sanitySlug/[slug].js
@@ -89,9 +89,9 @@ const Slug = ({
           included_segments: ['Subscribed Users'],
           // external_id: '514c1b62-cb15-4f13-a9dd-fb141d7fe215',
           contents: {en: 'New Order Has been placed', es: 'Spanish Message'},
-          headings:{"en": "New Order Placed", "es": "Spanish Title"},
+          headings:{en: "New Order Placed", "es": "Spanish Title"},
           name: 'New_Order',
-          url:'https://vsb.sanity.studio/desk/sanityorder',
+          url:'https://vsb.sanity.studio/desk/sanityorder'
           // big_picture:'https://img.onesignal.com/tmp/53a40efa-9a8b-431f-a034-04525b8bed60.png',
           // buttons:[{"id": "id1", "text": "first button", "icon": "ic_menu_share"}, {"id": "id2", "text": "second button", "icon": "ic_menu_send"}],
           // web_buttons:[{"id": "like-button", "text": "Like", "icon": "http://i.imgur.com/N8SN8ZS.png", "url": "https://vsb.vercel.app"}, {"id": "read-more-button", "text": "Read more", "icon": "http://i.imgur.com/MIxJp1L.png", "url": "https://vsb.vercel.app"}],
