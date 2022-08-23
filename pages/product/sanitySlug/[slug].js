b6a54be5ccf3e6fae8ca5f109b5a4b7a155609f0
PARV-Jains <2006parvjain@gmail.com>
2022-08-23 19:51:02 +0530

    filter

--- pages/product/sanitySlug/[slug].js
+++ pages/product/sanitySlug/[slug].js
@@ -86,7 +86,7 @@ const Slug = ({
         },
         body: JSON.stringify({
           app_id: '8c33f5fc-1004-4f36-9b24-0b17d3cbc7c6',
-          included_segments: ['Subscribed Users'],
+          include_player_ids: ["06dbb999-bc41-486b-a952-caa00f41ed97"],
           // external_id: '514c1b62-cb15-4f13-a9dd-fb141d7fe215',
           contents: {en: 'English New Order Has been Placed', es: 'Spanish Message'},
           name: 'Trial_CAMPAIGN_NAME',
